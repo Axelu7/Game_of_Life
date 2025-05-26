639b107b7a0b89da9bec56ee3135551cac42e4c3
Andrei Onete <oneteandreialex2006@gmail.com>
2025-05-26 20:40:21 +0300

    ref: Added Task 1 files after reinstalling the checker and modifying a few things in order for the refs to work again

---
+++ main1.c
@@ -0,0 +1,18 @@
+#include "header1.h"
+
+int main(int argc, char* argv[])
+{
+    int T, N, M, K;
+    char** mat;
+    
+    //Alocare dinamica a matricii de intrare
+    mat = read(&T, &N, &M, &K, argv[1]);
+    
+    //Verifica numarul task-ului
+    if(T == 1)
+        GoL(K, N, M, mat, argv[2]);
+
+    //Goleste memoria alocata dinamic din matricea de intrare
+    free_mem_mat(mat, N);
+    return 0;
+}
